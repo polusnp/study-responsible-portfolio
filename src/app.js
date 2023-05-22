bfca95e50321088ce96d3c90c55bfe7034f8a559
Pavlo Nahurnyi <polusnp@gmail.com>
2023-05-23 00:03:13 +0300

    #upgrade animation css

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ hambButton.addEventListener('click', toggleButton);
 // css animation for .hero-name-animate
 
 const element = document.querySelector('.hero-name-animate');
-element.classList.add('animate__animated', 'animate__shakeX');
+element.classList.add('animate__animated', 'animate__flipInX');
 
 document.documentElement.style.setProperty('--animate-duration', '1s');
 
